a25f64b06a66cb7b46afdae9442286d1444028bc
Frezworx <frezworx@gmail.com>
2024-12-18 19:27:11 +0200

    Use environment variables for settings and update serializer.
    Replaced hardcoded secret key in settings.py with an environment variable using dotenv for enhanced security. Updated the MovieSerializer to remove the 'max_length' constraint on the description field, allowing for greater flexibility.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,7 +6,7 @@ from cinema.models import Movie
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=255)
+    description = serializers.CharField()
     duration = serializers.IntegerField(required=True)
 
     def create(self, validated_data):
