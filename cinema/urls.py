b09015b6eaeb89c2bd53c1a77ec530e64d77c0e1
Frezworx <frezworx@gmail.com>
2024-12-18 19:04:31 +0200

    Add initial Movie model, DRF integration, and CRUD endpoints
    This commit creates the initial database migration for the `Movie` model. It integrates Django REST Framework and adds API endpoints for listing, retrieving, creating, updating, and deleting movies. Additionally, it updates project settings and URLs to support these changes.

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cinema.views import movies_list
+from cinema.views import movies_list, movies_detail
 
 app_name = "cinema"
 
 urlpatterns = [
-    path("movie/", )
+    path("movies/", movies_list, name="movies-list"),
+    path("movies/<int:pk>/", movies_detail, name="movies-detail"),
     ]
